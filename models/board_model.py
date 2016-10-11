682c40796e113c894e6c820f7ca5bd1a8e954641
chibbol <barbara.cegledi@gmail.com>
2016-10-11 14:47:11 +0200

    model skeletons done

--- models/board_model.py
+++ models/board_model.py
@@ -1,6 +1,5 @@
 from peewee import *
 from basemodel import BaseModel
 
-
 class Board(BaseModel):
     title = CharField(unique=True)
--- models/list_model.py
+++ models/list_model.py
@@ -2,6 +2,6 @@ from peewee import *
 from basemodel import BaseModel
 from board_model import Board
 
-class List(BaseModel):
-    board_owner = ForeignKeyField(Board, related_name="board_title")
+class ListModel(BaseModel):
+    owner_board = ForeignKeyField(Board, related_name="board_title")
     title = CharField(unique=True)
